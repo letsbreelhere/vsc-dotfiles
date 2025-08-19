81063a9ba84c33a543df70dc4a52eef2b57a09cf
letsbreelhere <letsbreelhere@gmail.com>
2025-08-19 11:27:27 -0700

    formatting

--- README.md
+++ README.md
@@ -1,3 +1,17 @@
 # Custom VS Code dotfiles
 
 Contains a few helper scripts for my personal use.
+
+# Installation
+
+To install:
+
+1. Create the package:
+
+```
+yarn vsce package
+```
+
+2. In VSCode, "Install extension from VSIX..."
+
+3. Done!
--- src/extension.ts
+++ src/extension.ts
@@ -1,106 +1,146 @@
+import { rename } from 'fs';
+import { pathToFileURL } from 'url';
 import * as vscode from 'vscode';
 
 export function activate(context: vscode.ExtensionContext) {
-	console.log('vsc-dotfiles is active');
+  console.log("vsc-dotfiles is active");
 
-	const wordUnderCursor = (): [string, vscode.Range] | [undefined, undefined] => {
-		const editor = vscode.window.activeTextEditor;
-		if (!editor) {
-			return [undefined, undefined];
-		}
-		const document = editor.document;
-		const position = editor.selection.active;
-		const wordRange = document.getWordRangeAtPosition(position);
-		if (!wordRange) {
-			return [undefined, undefined];
-		}
-		const word = document.getText(wordRange);
-		return [word, wordRange];
+	const activeEditorPath = (): string => {
+    const activeEditor = vscode.window.activeTextEditor;
+
+    if (activeEditor) {
+      const filePath = activeEditor.document.uri.fsPath;
+			return filePath;
+    } else {
+			throw new Error("No active editor");
+    }
 	};
 
-	context.subscriptions.push(
-		vscode.commands.registerCommand('vsc-dotfiles.searchWordUnderCursor', () => {
-			const [word, _] = wordUnderCursor();
-			if (!word) {
-				console.error('No word under cursor');
-				return;
-			}
+  const renameCurrentFile = async () => {
+		const path = activeEditorPath();
 
-			vscode.commands.executeCommand('workbench.action.findInFiles', {
-				query: word
-			});
-		})
+		const newPath = await vscode.window.showInputBox({
+			title: "New file name",
+			value: path,
+		});
+
+		if (newPath) {
+			await vscode.workspace.fs.rename(vscode.Uri.file(path), vscode.Uri.file(newPath));
+		}
+  };
+	context.subscriptions.push(
+    vscode.commands.registerCommand("vsc-dotfiles.renameCurrentFile", renameCurrentFile)
 	);
 
-	context.subscriptions.push(
-		vscode.commands.registerCommand('vsc-dotfiles.searchSelection', () => {
-			const selection = vscode.window.activeTextEditor?.selection;
-			if (!selection || selection.isEmpty) {
-				console.error('No selection');
-				return;
-			}
+  const wordUnderCursor = ():
+    | [string, vscode.Range]
+    | [undefined, undefined] => {
+    const editor = vscode.window.activeTextEditor;
+    if (!editor) {
+      return [undefined, undefined];
+    }
+    const document = editor.document;
+    const position = editor.selection.active;
+    const wordRange = document.getWordRangeAtPosition(position);
+    if (!wordRange) {
+      return [undefined, undefined];
+    }
+    const word = document.getText(wordRange);
+    return [word, wordRange];
+  };
 
-			const selectionRange = new vscode.Range(selection.start, selection.end);
-			const selectedText = vscode.window.activeTextEditor?.document.getText(selectionRange);
+  context.subscriptions.push(
+    vscode.commands.registerCommand(
+      "vsc-dotfiles.searchWordUnderCursor",
+      () => {
+        const [word, _] = wordUnderCursor();
+        if (!word) {
+          console.error("No word under cursor");
+          return;
+        }
 
-			if (!selectedText) {
-				console.error('No selected text');
-				return;
-			}
+        vscode.commands.executeCommand("workbench.action.findInFiles", {
+          query: word,
+        });
+      }
+    )
+  );
 
-			vscode.commands.executeCommand('workbench.action.findInFiles', {
-				query: selectedText
-			});
-		})
-	);
+  context.subscriptions.push(
+    vscode.commands.registerCommand("vsc-dotfiles.searchSelection", () => {
+      const selection = vscode.window.activeTextEditor?.selection;
+      if (!selection || selection.isEmpty) {
+        console.error("No selection");
+        return;
+      }
 
-	context.subscriptions.push(
-		vscode.commands.registerCommand('vsc-dotfiles.toggleCasing', () => {
-			let replacingSelection = false;
-			let range: vscode.Range | undefined = undefined;
-			const editor = vscode.window.activeTextEditor;
-			if (!editor) {
-				return;
-			}
-			let selectedText: string | undefined;
-			if (editor.selection.isEmpty) {
-				[selectedText, range] = wordUnderCursor();
-			} else {
-				selectedText = editor.document.getText(editor.selection);
-				replacingSelection = true;
-			}
+      const selectionRange = new vscode.Range(selection.start, selection.end);
+      const selectedText =
+        vscode.window.activeTextEditor?.document.getText(selectionRange);
 
-			if (!selectedText) {
-				console.error('No selection or word under cursor');
-				return;
-			}
+      if (!selectedText) {
+        console.error("No selected text");
+        return;
+      }
 
-			const isSnakeCase = (str: string) => str.includes('_');
-			const toCamelCase = (str: string) =>
-				str.replace(/_([a-zA-Z])/g, (_, c) => c.toUpperCase()).replace(/^([a-z])/, (m) => m.toUpperCase());
-			const toSnakeCase = (str: string) =>
-				str.replace(/([A-Z])/g, '_$1').replace(/^_/, '').toLowerCase();
+      vscode.commands.executeCommand("workbench.action.findInFiles", {
+        query: selectedText,
+      });
+    })
+  );
 
-			let toggled: string;
-			if (isSnakeCase(selectedText)) {
-				toggled = toCamelCase(selectedText);
-			} else {
-				toggled = toSnakeCase(selectedText);
-			}
+  context.subscriptions.push(
+    vscode.commands.registerCommand("vsc-dotfiles.toggleCasing", () => {
+      let replacingSelection = false;
+      let range: vscode.Range | undefined = undefined;
+      const editor = vscode.window.activeTextEditor;
+      if (!editor) {
+        return;
+      }
+      let selectedText: string | undefined;
+      if (editor.selection.isEmpty) {
+        [selectedText, range] = wordUnderCursor();
+      } else {
+        selectedText = editor.document.getText(editor.selection);
+        replacingSelection = true;
+      }
 
-			let selection: vscode.Selection = editor.selection;
-			if (!replacingSelection) {
-				if (range) {
-					selection = new vscode.Selection(range.start, range.end);
-				} else {
-					vscode.window.showErrorMessage('No range available for selection');
-				}
-			}
-			editor.edit(editBuilder => {
-				editBuilder.replace(selection, toggled);
-			});
-		})
-	);
+      if (!selectedText) {
+        console.error("No selection or word under cursor");
+        return;
+      }
+
+      const isSnakeCase = (str: string) => str.includes("_");
+      const toCamelCase = (str: string) =>
+        str
+          .replace(/_([a-zA-Z])/g, (_, c) => c.toUpperCase())
+          .replace(/^([a-z])/, (m) => m.toUpperCase());
+      const toSnakeCase = (str: string) =>
+        str
+          .replace(/([A-Z])/g, "_$1")
+          .replace(/^_/, "")
+          .toLowerCase();
+
+      let toggled: string;
+      if (isSnakeCase(selectedText)) {
+        toggled = toCamelCase(selectedText);
+      } else {
+        toggled = toSnakeCase(selectedText);
+      }
+
+      let selection: vscode.Selection = editor.selection;
+      if (!replacingSelection) {
+        if (range) {
+          selection = new vscode.Selection(range.start, range.end);
+        } else {
+          vscode.window.showErrorMessage("No range available for selection");
+        }
+      }
+      editor.edit((editBuilder) => {
+        editBuilder.replace(selection, toggled);
+      });
+    })
+  );
 }
 
 // This method is called when your extension is deactivated
